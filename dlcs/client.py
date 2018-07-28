87088f3d9bfa1807c89740d9a5239e578970426e
tomcrane <tom@nanoplanet.com>
2018-07-28 15:14:53 +0100

    retrieve single image

--- dlcs/client.py
+++ dlcs/client.py
@@ -1,14 +1,32 @@
 from dlcs.queue_response import Batch
-from requests import post, auth
+from requests import get, post, auth
 import settings
 
 
 def register_collection(image_collection):
 
-    authorisation = auth.HTTPBasicAuth(settings.DLCS_API_KEY, settings.DLCS_API_SECRET)
-    url = settings.DLCS_ENTRY + 'customers/' + str(settings.DLCS_CUSTOMER_ID) + '/queue'
+    url = get_customer_url() + 'queue'
     json = image_collection.as_json()
-    response = post(url, data=json, auth=authorisation)
+    response = post(url, data=json, auth=get_authorisation())
     batch = Batch(response.json())
 
     return batch
+
+def get_image(image_slug, space=None):
+
+    if space == None:
+        space = settings.DLCS_SPACE
+    
+    url = get_customer_url + "spaces/" + str(space) + "/images/" + image_slug
+    response = get(url, auth=get_authorisation())
+    # just do this? Or use the Image class somehow?
+    return response.json()
+    
+def get_authorisation():
+
+    return auth.HTTPBasicAuth(settings.DLCS_API_KEY, settings.DLCS_API_SECRET)
+
+def get_customer_url():
+
+    cust_id = str(settings.DLCS_CUSTOMER_ID)
+    return settings.DLCS_ENTRY + 'customers/' + cust_id + "/"
